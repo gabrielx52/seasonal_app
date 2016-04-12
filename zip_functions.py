9b85408fff8f9b52ddbaa864d30abb2bf2e16d5c
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-04-12 13:51:20 -0700

    added city list and code list functions

--- zip_functions.py
+++ zip_functions.py
@@ -2,35 +2,32 @@ import zipcode
 import re
 
 
-# def zip_code_finder(starting_zip='98119', mile_radius=100):
-#     """returns dict of zipcodes within radius of starting zip"""
-#     zip_dict = {}
-#     zip_obj = zipcode.isequal(starting_zip)
-#     local_zips = zipcode.isinradius((zip_obj.lat, zip_obj.lon), mile_radius)
-#     for i in local_zips:
-#         state_city = i.state + ' ' + i.city
-#         if state_city not in zip_dict:
-#             zip_dict[state_city] = [i.zip]
-#         zip_dict[state_city].append(i.zip)
-#     return zip_dict
-#
-#
-# print(zip_code_finder())
-
-
-
 def local_city_list_maker(starting_zip='98119', mile_radius=100):
     """returns list of cities within radius of starting zip"""
-    city_code_list = []
+    city_list = []
     zip_obj = zipcode.isequal(starting_zip)
     local_zips = zipcode.isinradius((zip_obj.lat, zip_obj.lon), mile_radius)
     for i in local_zips:
         state_city = i.state + ' ' + i.city
-        if state_city not in city_code_list:
-            city_code_list.append(state_city)
-    return city_code_list
-
-print(local_city_list_maker())
+        if state_city not in city_list:
+            city_list.append(state_city)
+    return city_list
 
 
-def 
+def weather_station_code_finder(city_list):
+    """returns list of weather station ICAO codes
+    city_list: list of states and cities * needs to be in 'ST CITY' format
+               use return from local_city_list_maker()
+    """
+    city_code_list = []
+    code_db = open('weather_codes.txt', 'r').read()
+    for city in city_list:
+        city_info = re.search(city + '.*[PKXQ]\w{3}\s\s', code_db)
+        try:
+            code = re.search('\s[PKXQ]\w{3}\s\s', city_info.group())
+            city_code_list.append((city, code.group()))
+        except:
+            pass
+    print(city_code_list)
+
+weather_station_code_finder(local_city_list_maker())
