d2a00c3cc7b2543f2a65044a2f2e3f6b75271095
Gabriel Meringolo <gabriel.meringolo@gmail.com>
2016-04-14 08:52:39 -0700

    refactoring

--- zip_functions.py
+++ zip_functions.py
@@ -1,38 +1,38 @@
 import zipcode
 import re
 import requests
+import pprint
+import logging
 
-def local_city_list_maker(starting_zip='98119', mile_radius=100):
-    """returns list of cities within radius of starting zip"""
-    city_list = []
+
+
+
+def local_city_grabber(starting_zip='98119', mile_radius=100):
+    """returns set of cities within radius of starting zip"""
     zip_obj = zipcode.isequal(starting_zip)
     local_zips = zipcode.isinradius((zip_obj.lat, zip_obj.lon), mile_radius)
-    for i in local_zips:
-        state_city = i.state + ' ' + i.city
-        if state_city not in city_list:
-            city_list.append(state_city)
-    return city_list
+    return {'{} {}'.format(i.state, i.city) for i in local_zips}
 
 
-def weather_station_code_finder(city_list=local_city_list_maker()):
+def weather_station_code_grabber(cities=local_city_grabber()):
     """returns list of weather station ICAO codes
     city_list: list of states and cities * needs to be in 'ST CITY' format
                use return from local_city_list_maker()
     """
-    city_code_list = []
-    code_db = open('weather_codes.txt', 'r').read()
-    for city in city_list:
+    city_codes = []
+    with open('weather_codes.txt', 'r') as text:
+        code_db = text.read()
+    for city in cities:
         city_info = re.search(city + '.*[PKXQ]\w{3}\s\s', code_db)
         try:
             code = re.search('\s[PKXQ]\w{3}\s\s', city_info.group())
-            #city_code_list.append((city, code.group()))
-            city_code_list.append((code.group()).strip())
+            city_codes.append((code.group()).strip())
         except:
             pass
-    return city_code_list
+    return city_codes
 
-
-print(weather_station_code_finder(['OK MUSKOGEE'])[0])
+#pprint.pprint(local_city_grabber('06424'))
+pprint.pprint(weather_station_code_grabber())
 
 # #starting to get weather data. need to make mock produce data for cross reference
 # codes = weather_station_code_finder()
